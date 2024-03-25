7678ead862b8902e36d9270205eb59449a869eb5
Merve Başak DEMİRTAŞ <72447846+mervejss@users.noreply.github.com>
2024-03-26 02:21:25 +0300

    Add files via upload

--- src/app/admin-ana-sayfa/admin-ana-sayfa.component.ts
+++ src/app/admin-ana-sayfa/admin-ana-sayfa.component.ts
@@ -3,21 +3,23 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { AdminService } from '../services/admin.service';
 
-
 @Component({
   selector: 'app-admin-ana-sayfa',
   templateUrl: './admin-ana-sayfa.component.html',
   styleUrls: ['./admin-ana-sayfa.component.scss'],
 })
 export class AdminAnaSayfaComponent implements OnInit {
+  adminData: any;
+  
+
 
   constructor(private http: HttpClient, private router: Router, private _auth: AdminService) {}
-
+  
 
   ngOnInit(): void {
+    this.adminData = this._auth.getAdminData();
 
   }
-  
   
 
   yeniAnketSorusuEkle(): void {
--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -14,8 +14,12 @@ export class AdminService {
   private _registerUrl = "http://localhost:3000/api/admins"
   private _loginUrl = "http://localhost:3000/api/adminlogin"
   private _adminInfoUrl = "http://localhost:3000/api/admininfo"
+  _adminData: any;
 
-  constructor(private http: HttpClient,private _router: Router) {}
+  constructor(private http: HttpClient,private _router: Router,) {
+    this._adminData = JSON.parse(localStorage.getItem('adminData') || '{}');
+
+  }
 
  
   registerUser(user: any)
@@ -47,6 +51,13 @@ export class AdminService {
     return !!localStorage.getItem('token')    
   }
   
- 
-  
+  setAdminData(data: any) {
+    this._adminData = data;
+    // adminData'yı localStorage'a kaydet
+    localStorage.setItem('adminData', JSON.stringify(data));
+  }
+
+  getAdminData() {
+    return this._adminData;
+  }
 }
